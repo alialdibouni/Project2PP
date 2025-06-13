972c1fa32f8264d894d7c7df04c31cf17a75646c
Ali Al-Dibouni <a.al.dibouni@gmail.com>
2025-06-13 17:23:35 +0100

    Added simple camera movement
    Camera can look in 360 degrees, and up and down 90 degrees. The sensitivity and yaw can be changed

---
+++ Assets/2PP Test/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Mouse Look Settings")]
+    public float mouseSensitivity = 100f;
+    public bool lockCursor = true;
+
+    [Header("Camera Rotation Limits")]
+    public float minPitch = -90f;
+    public float maxPitch = 90f;
+
+    [SerializeField] private float yaw = 0f;
+    [SerializeField] private float pitch = 0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        yaw = angles.y;
+        pitch = angles.x;
+
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        HandleMouseLook();
+        HandleCursorToggle();
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        yaw += mouseX;
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+
+    void HandleCursorToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (lockCursor && Cursor.lockState != CursorLockMode.Locked)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
