5ff0775472e072657d04012260ae0b8319e2508e
Ali Al-Dibouni <a.al.dibouni@gmail.com>
2025-07-17 16:43:10 +0100

    Added flags in maze

--- Assets/2PP Test/Scripts/CameraSwitcher.cs
+++ Assets/2PP Test/Scripts/CameraSwitcher.cs
@@ -13,49 +13,61 @@ public class CameraSwitcher : MonoBehaviour
 
     void Start()
     {
-        ActivateCamera(currentCameraIndex);
-        ActivateCinemachineCamera(currentCinemachineIndex);
+        if (cameras != null && cameras.Length > 0)
+            ActivateCamera(currentCameraIndex);
+        if (cinemachineCameras != null && cinemachineCameras.Length > 0)
+            ActivateCinemachineCamera(currentCinemachineIndex);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchToCamera(0);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchToCamera(1);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchToCamera(2);
-        if (Input.GetKeyDown(KeyCode.Alpha4)) SwitchToCamera(3);
-        if (Input.GetKeyDown(KeyCode.Alpha5)) SwitchToCamera(4);
-        if (Input.GetKeyDown(KeyCode.Alpha6)) SwitchToCamera(5);
-
-        if (Input.GetKeyDown(KeyCode.Tab))
+        // Camera switching by number keys
+        if (cameras != null && cameras.Length > 0)
         {
-            currentCameraIndex = (currentCameraIndex + 1) % cameras.Length;
-            ActivateCamera(currentCameraIndex);
+            if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchToCamera(0);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchToCamera(1);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchToCamera(2);
+            if (Input.GetKeyDown(KeyCode.Alpha4)) SwitchToCamera(3);
+            if (Input.GetKeyDown(KeyCode.Alpha5)) SwitchToCamera(4);
+            if (Input.GetKeyDown(KeyCode.Alpha6)) SwitchToCamera(5);
+
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                currentCameraIndex = (currentCameraIndex + 1) % cameras.Length;
+                ActivateCamera(currentCameraIndex);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchToCinemachineCamera(0);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchToCinemachineCamera(1);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchToCinemachineCamera(2);
-        if (Input.GetKeyDown(KeyCode.Alpha4)) SwitchToCinemachineCamera(3);
-        if (Input.GetKeyDown(KeyCode.Alpha5)) SwitchToCinemachineCamera(4);
-        if (Input.GetKeyDown(KeyCode.Alpha6)) SwitchToCinemachineCamera(5);
-
-        if (Input.GetKeyDown(KeyCode.Tab))
+        // Cinemachine camera switching by number keys
+        if (cinemachineCameras != null && cinemachineCameras.Length > 0)
         {
-            currentCinemachineIndex = (currentCinemachineIndex + 1) % cinemachineCameras.Length;
-            ActivateCinemachineCamera(currentCinemachineIndex);
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchToCinemachineCamera(0);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchToCinemachineCamera(1);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchToCinemachineCamera(2);
+            if (Input.GetKeyDown(KeyCode.Alpha4)) SwitchToCinemachineCamera(3);
+            if (Input.GetKeyDown(KeyCode.Alpha5)) SwitchToCinemachineCamera(4);
+            if (Input.GetKeyDown(KeyCode.Alpha6)) SwitchToCinemachineCamera(5);
 
-        // Check for Animator on active Cinemachine camera and toggle on J press
-        Animator animator = GetActiveCinemachineAnimator();
-        if (animator != null && Input.GetKeyDown(KeyCode.J))
-        {
-            animator.enabled = !animator.enabled;
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                currentCinemachineIndex = (currentCinemachineIndex + 1) % cinemachineCameras.Length;
+                ActivateCinemachineCamera(currentCinemachineIndex);
+            }
+
+            // Check for Animator on active Cinemachine camera and toggle on J press
+            Animator animator = GetActiveCinemachineAnimator();
+            if (animator != null && Input.GetKeyDown(KeyCode.J))
+            {
+                animator.enabled = !animator.enabled;
+            }
         }
     }
 
     private Animator GetActiveCinemachineAnimator()
     {
-        if (currentCinemachineIndex >= 0 && currentCinemachineIndex < cinemachineCameras.Length)
+        if (cinemachineCameras != null &&
+            currentCinemachineIndex >= 0 &&
+            currentCinemachineIndex < cinemachineCameras.Length)
         {
             return cinemachineCameras[currentCinemachineIndex].GetComponent<Animator>();
         }
@@ -64,7 +76,7 @@ public class CameraSwitcher : MonoBehaviour
 
     void SwitchToCamera(int index)
     {
-        if (index >= 0 && index < cameras.Length)
+        if (cameras != null && index >= 0 && index < cameras.Length)
         {
             currentCameraIndex = index;
             ActivateCamera(currentCameraIndex);
@@ -81,7 +93,7 @@ public class CameraSwitcher : MonoBehaviour
 
     void SwitchToCinemachineCamera(int index)
     {
-        if (index >= 0 && index < cinemachineCameras.Length)
+        if (cinemachineCameras != null && index >= 0 && index < cinemachineCameras.Length)
         {
             currentCinemachineIndex = index;
             ActivateCinemachineCamera(currentCinemachineIndex);
@@ -92,23 +104,13 @@ public class CameraSwitcher : MonoBehaviour
     { 
         for (int i = 0; i < cinemachineCameras.Length; i++)
         {
-            /*bool isActive = (i == index);
-            cinemachineCameras[i].SetActive(isActive);
-
-            Animator animator = cinemachineCameras[i].GetComponent<Animator>();
-            if (animator != null)
-            {
-                animator.enabled = isActive;
-            }*/
-
-            cinemachineCameras[i].SetActive(i == index); // old code
-
+            cinemachineCameras[i].SetActive(i == index);
         }
     }
 
     public void ActivateCinemachineCameraByIndex(int index)
     {
-        if (index >= 0 && index < cinemachineCameras.Length)
+        if (cinemachineCameras != null && index >= 0 && index < cinemachineCameras.Length)
         {
             currentCinemachineIndex = index;
             ActivateCinemachineCamera(currentCinemachineIndex);
@@ -117,7 +119,7 @@ public class CameraSwitcher : MonoBehaviour
 
     public void ActivateCameraByIndex(int index)
     {
-        if (index >= 0 && index < cameras.Length)
+        if (cameras != null && index >= 0 && index < cameras.Length)
         {
             currentCameraIndex = index;
             ActivateCamera(currentCameraIndex);
