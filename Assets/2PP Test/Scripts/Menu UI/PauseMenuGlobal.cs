aa3f79126b0721fb5b7277240bc828d1684aef5a
Ali Al-Dibouni <a.al.dibouni@gmail.com>
2025-07-21 16:05:35 +0100

    Added Scenario Menu screen to car scenario

---
+++ Assets/2PP Test/Scripts/Menu UI/PauseMenuGlobal.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuGlobal : MonoBehaviour
+{
+
+    [SerializeField] GameObject pauseMenu;
+
+
+
+    public void Start()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; // Pause the game by setting time scale to 0
+        //show mouse cursor
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; // Pause the game by setting time scale to 0
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu"); // Load the main menu scene
+        Time.timeScale = 1f; // Reset time scale to normal
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        //hide mouse cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Restart()
+    {
+        pauseMenu.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Restart the current scene
+        Time.timeScale = 1f; // Reset time scale to normal
+    }
+
+    public void ShowDeathScreen()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ShowVictoryScreen()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
