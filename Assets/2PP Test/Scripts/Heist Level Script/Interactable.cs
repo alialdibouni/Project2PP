6943dc3a2658e7cf699a948606767a85959c8a86
Ali Al-Dibouni <a.al.dibouni@gmail.com>
2025-08-27 17:10:18 +0100

    Created event systems

--- Assets/2PP Test/Scripts/Heist Level Script/Interactable.cs
+++ Assets/2PP Test/Scripts/Heist Level Script/Interactable.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-    // Message to display when the player can interact with this object
-    public string promptMessage;
+    //Add or remove an interactionevent component to this object
+    public bool useEvents;
 
+    // Message to display when the player can interact with this object
+    [SerializeField]public string promptMessage;
+
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
 
     //This function is called by the player when they interact with this object
     public void BaseInteract()
     {
+        if(useEvents)
+            GetComponent<InteractionEvent>().onInteract.Invoke();
         Interact();
     }
 
