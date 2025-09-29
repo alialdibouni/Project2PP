ccb926a29956c5d74eacb0842611148b6df28dd5
Ali Al-Dibouni <a.al.dibouni@gmail.com>
2025-09-29 15:20:58 +0100

    Changes to report progress ui text

--- Assets/2PP Test/Scripts/Journalist Script/ReportProgressUI.cs
+++ Assets/2PP Test/Scripts/Journalist Script/ReportProgressUI.cs
@@ -22,10 +22,13 @@ public class ReportProgressUI : MonoBehaviour
         int total = ReporterTriggerBox.TotalReports;
         int done = ReporterTriggerBox.CompletedReports;
         // If you want "remaining" instead, use: int remaining = total - done;
-        reportsText.text = $"Reports completed: {done}/{total}"; 
         if(done >= total)
         {
             reportsText.text += "\nAll reports completed!\n Return to the News Van";
+        }
+        else
+        {
+            reportsText.text = $"Reports completed: {done}/{total}";
         }
     }
 }
